7570b2ed3e3442aaea92abb67bb63d6f7de02f51
Antonio <eaglevision1120@gmail.com>
2022-10-22 00:14:16 +0300

    portfolio

---
+++ src/constant/code-challenges-page/challenges/common/web-calculator.js
@@ -0,0 +1,23 @@
+import { ExternalLinkSquareAlt, Github, Vuejs } from "@vicons/fa"
+export default {
+    title: "Web Calculator",
+    des: "<p>Calculator is a fascinating computer. People Have been using it for computing a lot of stuff. It's been a useful tool for everyone. So I created my simple calculator using Vue.</p>",
+    links: [
+        {
+            link: "https://github.com/BroAntonio/MyPortFolio/tree/master/calculator-vue",
+            icon: Github,
+            tooltip: 'Open In Github'
+        },
+        {
+            link: "https://calculator.Antonio.com",
+            icon: ExternalLinkSquareAlt,
+            tooltip: 'Open Link'
+        }
+    ],
+    techs: [
+        {
+            name: "vue.js",
+            icon: Vuejs
+        }
+    ]
+}
---
+++ src/constant/social-network.js
@@ -0,0 +1,29 @@
+
+export const socialMediaLinks = [{
+    icon: `<i class='bx bxl-facebook' ></i>`,
+    title: "Facebook",
+    link: 'https://www.facebook.com//',
+},
+{
+    icon: `<i class='bx bxl-youtube' ></i>`,
+    title: "Youtube",
+    link: 'https://www.youtube.com/channel/UCNANDtTF63UTRcYioVsSCdA'
+},
+{
+    icon: `<i class='bx bxl-linkedin' ></i>`,
+    title: "LinkedIn",
+    link: 'https://www.linkedin.com/in/Antonio/'
+},
+{
+    icon: `<i class='bx bxl-github' ></i>`,
+    title: "GitHub",
+    link: 'https://github.com/BroAntonio'
+},
+{
+    icon: `<i class='bx bxl-twitter' ></i>`,
+    title: "Twitter",
+    link: 'https://twitter.com/broAntonio'
+}
+];
+
+export const email = "Antonio936@gmail.com";
